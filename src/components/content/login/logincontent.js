bfaa654a0eb0d7ea7642021bba5722ec6bb3b4e2
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2020-12-04 14:14:56 +0700

    3

--- src/components/content/login/logincontent.js
+++ src/components/content/login/logincontent.js
@@ -1,10 +1,37 @@
-
+import axios from 'axios';
 import { Component } from 'react';
+import {PostData} from './PostData';
+
 
 class Logincontent extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            email:'',
+            password:''
+        }
+        this.login = this.login.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+    
+
+    login(){
+      PostData('login', this.state).then ((result) =>{
+        let reponseJson = result;
+        console.log(reponseJson);
+
+      });
+    }
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value});
+        // console.log(this.state);
+    }
+    
     render() {
+
         return (
-            <div>
+ 
+        <div>
                 <div className="section" data-aos="fade-up">
                     <div className="container">
                         <div className="row justify-content-center mb-5">
@@ -15,14 +42,14 @@ class Logincontent extends Component {
                         </div>
                         <div className="row justify-content-center">
                             <div className="col-md-10 p-5 form-wrap">
-                                <form action="#">
+                                {/* <form action=""> */}
                                     <div className="row mb-4">
                                         <div className="form-group col-md-12">
                                             {/* <label htmlFor="email" className="label" Style="text-align:left">Email 1</label> */}
                                             <p htmlFor="email" className="label" Style="text-align:left">Email</p>
                                             <div className="form-field-icon-wrap">
                                                 <span className="icon ion-email"></span>
-                                                <input type="text" className="form-control" id="email" />
+                                                <input type="text" className="form-control" name="email" onChange={this.onChange}/>
                                             </div>
                                         </div>
                                         <div className="form-group col-md-12">
@@ -30,17 +57,18 @@ class Logincontent extends Component {
                                             <p htmlFor="Password" className="label" Style="text-align:left">Password</p>
                                             <div className="form-field-icon-wrap">
                                                 {/*<span className="icon ion-android-call"></span>*/}
-                                                <input type="password" className="form-control" id="password" />
+                                                <input type="password" className="form-control" name="password" onChange={this.onChange}/>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div className="row justify-content-center">
                                         <div className="col-md-4">
-                                            <input type="submit" className="btn btn-primary btn-outline-primary btn-block" value="LOGIN" />
+                                            <input type="submit" className="btn btn-primary btn-outline-primary btn-block" value="LOGIN" onClick={this.login}/>
+                                            {/* <button  onClick={()=>{this.login()}}>LOGIN</button> */}
                                         </div>
                                     </div>
-                                </form>
+                                {/* </form> */}
                             </div>
                         </div>
                     </div>
