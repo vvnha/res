a68a5a00deb0eb489fee00d5b5bb8f3f619b154f
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2020-12-05 23:42:31 +0700

    Update logout.js

--- src/components/content/login/logout.js
+++ src/components/content/login/logout.js
@@ -5,22 +5,21 @@ import { Link } from 'react-router-dom';
 
 class logout extends Component {
     render() {
-        if(localStorage.getItem('token')==null){
-            return <Redirect to="/" />
-        }else{
-            let token = JSON.parse(localStorage.getItem('token'));
-            const api = `https://restaurantqn.herokuapp.com/api/user`;
-            axios.get(api, { headers: {"Authorization" : `Bearer ${token}`} })
-            .then(res => {
-                // console.log(res.message);
-                localStorage.removeItem('token');
-                return <Redirect to="/" />
-                return <Redirect to="/" />
-            }).catch(err => {
-                    localStorage.removeItem('token');
-                    return <Redirect to="/" />
-                });
-        }
+        // if(localStorage.getItem('token')==null){
+        //     return <Redirect to="/" />
+        // }else{
+        //     let token = JSON.parse(localStorage.getItem('token'));
+        //     const api = `https://restaurantqn.herokuapp.com/api/user`;
+        //     axios.get(api, { headers: {"Authorization" : `Bearer ${token}`} })
+        //     .then(res => {
+        //         // console.log(res.message);
+        //         localStorage.removeItem('token');
+        //         return <Redirect to="/" />
+        //     }).catch(err => {
+        //             localStorage.removeItem('token');
+        //             return <Redirect to="/" />
+        //         });
+        // }
         return (
             <div></div>
         );
