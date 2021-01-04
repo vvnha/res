5380d58d95a659e2bc4ff934a5b58a56523a2fa1
vvnha <vvnha.17it3@sict.dn.vn>
2021-01-04 22:05:10 +0700

    update edit user

--- src/components/content/profile/profile.js
+++ src/components/content/profile/profile.js
@@ -3,53 +3,54 @@ import Footer from '../../footer/footer';
 import Header from '../../header/header';
 import Infor from './infor';
 import './cssfile.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class Profile extends Component {
     render() {
         return (
-            
+
             <div>
                 <Header />
-                <div className="container" style={{ marginTop:100+'px' }}> 
-                    <div className="container"> 
-                        <div className="row profile">        
-                            <div className="col-md-3">          
-                                <div className="profile-sidebar">                          
+                <div className="container" style={{ marginTop: 100 + 'px' }}>
+                    <div className="container">
+                        <div className="row profile">
+                            <div className="col-md-3">
+                                <div className="profile-sidebar">
                                     <div className="profile-userpic">
-                                        <img src="https://hocwebgiare.com/thiet_ke_web_chuan_demo/bootstrap_user_profile/images/profile_user.jpg" className="img-responsive" />               
-                                    </div>                                            
-                                    <div className="profile-usertitle">                   
-                                        <div className="profile-usertitle-name">Khách hàng</div>                  
-                                        <div className="profile-usertitle-job">TEST</div>              
-                                    </div>                                                
-                                    <div className="profile-userbuttons">                 
-                                        <button type="button" className="btn btn-success btn-sm">Trang chủ</button>                  
-                                        <button type="button" className="btn btn-danger btn-sm">Thoát ra</button>                
-                                    </div>                                            
-                                    <div className="profile-usermenu">                    
-                                        <ul className="nav">                     
-                                            <li className="active"><a href=""><i className="fa fa-info-circle"></i>Cập nhật thông tin cá nhân </a>                     
-                                            </li>                                            
+                                        <img src="https://hocwebgiare.com/thiet_ke_web_chuan_demo/bootstrap_user_profile/images/profile_user.jpg" className="img-responsive" />
+                                    </div>
+                                    <div className="profile-usertitle">
+                                        <div className="profile-usertitle-name">Khách hàng</div>
+                                        <div className="profile-usertitle-job">TEST</div>
+                                    </div>
+                                    <div className="profile-userbuttons">
+                                        <button type="button" className="btn btn-success btn-sm">Trang chủ</button>
+                                        <Link to="/user" type="button" className="btn btn-warning btn-sm">Chinh sua</Link>
+                                    </div>
+                                    <div className="profile-usermenu">
+                                        <ul className="nav">
+                                            <li className="active"><a href=""><i className="fa fa-info-circle"></i>Cập nhật thông tin cá nhân </a>
+                                            </li>
                                             <li>
-                                                <a href=""> <i className="glyphicon glyphicon-shopping-cart"></i> Quản lý đơn hàng </a>                   
-                                            </li> 
+                                                <a href=""> <i className="glyphicon glyphicon-shopping-cart"></i> Quản lý đơn hàng </a>
+                                            </li>
                                             <li>
-                                                <a href=""> <i className="fa fa-question-circle-o"></i>Phản hồi</a>                       
-                                            </li>                                         
-                                        </ul>                
-                                    </div>                            
-                                </div>     
-                            </div>    
+                                                <a href=""> <i className="fa fa-question-circle-o"></i>Phản hồi</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
 
-                            <div className="col-md-9"> 
+                            <div className="col-md-9">
                                 <div className="profile-content">
-                                <Infor />
-                              
-                            </div>   
-                              
-                        </div>  
+                                    <Infor />
+
+                                </div>
+
+                            </div>
                         </div>
-                    </div> 
+                    </div>
                 </div>
                 <Footer />
             </div>
@@ -77,4 +78,4 @@ export default Profile;
         </div>
     </div>
 </div > */}
-{/* <div classNameName="map-wrap" id="map" data-aos="fade"></div> */}
+{/* <div classNameName="map-wrap" id="map" data-aos="fade"></div> */ }
--- src/components/content/user/userInfo.js
+++ src/components/content/user/userInfo.js
@@ -1,12 +1,47 @@
 import { Component } from 'react';
+import callApi from '../utils/apiCaller';
 
 class UserInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isLoading: false,
-            user: []
+            user: [],
+            name: '',
+            email: '',
+            phone: ''
         };
+    }
+    componentDidMount() {
+        this.setState({
+            isLoading: true,
+        });
+
+        if (localStorage.getItem('token')) {
+            var token = localStorage.getItem('token');
+            callApi('api/user', 'GET', null, { 'Authorization': `Bearer ${JSON.parse(token)}` }).then(res => {
+                if (res) {
+                    var data = res.data;
+                    this.setState({
+                        user: data,
+                        isLoading: false,
+                        name: data.name,
+                        email: data.email,
+                        phone: data.phone
+                    });
+                } else {
+                    localStorage.removeItem('token');
+                    console.log(res + "--Erro token!");
+                }
+            });
+            this.setState({
+                isLoading: false,
+            });
+        } else {
+            this.setState({
+                isLoading: false,
+            });
+        }
     }
     render() {
         return (
@@ -25,7 +60,7 @@ class UserInfo extends Component {
                                     <label htmlFor="name" className="label">Name</label>
                                     <div className="form-field-icon-wrap">
                                         <span className="icon ion-android-person"></span>
-                                        <input type="text" className="form-control" id="name" />
+                                        <input type="text" className="form-control" id="name" name="name" onChange={this.onChange} defaultValue={this.state.user.name ? this.state.user.name : ''} />
                                     </div>
                                 </div>
                             </div>
@@ -34,7 +69,7 @@ class UserInfo extends Component {
                                     <label htmlFor="email" className="label">Email</label>
                                     <div className="form-field-icon-wrap">
                                         <span className="icon ion-email"></span>
-                                        <input type="email" className="form-control" id="email" />
+                                        <input type="email" className="form-control" id="email" name="email" onChange={this.onChange} defaultValue={this.state.user.email ? this.state.user.email : ''} />
                                     </div>
                                 </div>
                             </div>
@@ -43,13 +78,12 @@ class UserInfo extends Component {
                                     <label htmlFor="phone" className="label">Phone</label>
                                     <div className="form-field-icon-wrap">
                                         <span className="icon ion-android-call"></span>
-                                        <input type="text" className="form-control" id="phone" />
+                                        <input type="text" className="form-control" id="phone" name="phone" onChange={this.onChange} defaultValue={this.state.user.phone ? this.state.user.phone : ''} />
                                     </div>
                                 </div>
                             </div>
                             <div className="row justify-content-center">
                                 <div className="col-md-4">
-
                                     <a href="" className="btn btn-primary btn-outline-primary btn-block" onClick={this.onSubmit}>SUBMIT</a>
                                 </div>
                             </div>
@@ -58,6 +92,33 @@ class UserInfo extends Component {
                 </div>
             </div >
         );
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        var { name, email, phone, user } = this.state;
+        var body = {
+            name: name,
+            email: email,
+            phone: phone
+        }
+        if (name !== '' && email !== '' && phone !== '') {
+            if (localStorage.getItem('token')) {
+                var token = localStorage.getItem('token');
+                callApi(`api/user/${user.id}`, 'PATCH', body, { 'Authorization': `Bearer ${JSON.parse(token)}` }).then(res => {
+                    if (res) {
+                        alert('sua nguoi dung thanh cong');
+                    }
+                });
+            }
+        }
+    }
+    onChange = (e) => {
+        var target = e.target;
+        var name = target.name;
+        var value = target.type === 'checkbox' ? target.checked : target.value;
+        this.setState({
+            [name]: value
+        });
     }
 }
 export default UserInfo;
