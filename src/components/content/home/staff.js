b411e1689f38eac3e3b66acf1e6eea239a7b96d7
vvnha <vvnha.17it3@sict.dn.vn>
2021-01-20 23:28:35 +0700

    oke

--- src/components/content/home/staff.js
+++ src/components/content/home/staff.js
@@ -15,7 +15,7 @@ class Staff extends Component {
                                     <blockquote className="testimonial">
                                         <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quaerat recusandae molestias incidunt sapiente sit numquam delectus mollitia! Non laudantium impedit voluptas consequatur corrupti. Cumque consequuntur nemo eos et error!&rdquo;</p>
                                         <div className="author">
-                                            <img src="default/img/person_1.jpg" alt="Image placeholder" className="mb-3" />
+                                            <img src="default/img/person_1.jpg" alt="Image placeholder" className="mb-3" style={{ width: 'auto' }} />
                                             <h4>Maxim Smith</h4>
                                             <p>CEO, Founder</p>
                                         </div>
@@ -25,7 +25,7 @@ class Staff extends Component {
                                     <blockquote className="testimonial">
                                         <p>&ldquo;Sint adipisci laborum dolorum ipsa quidem alias ipsum aperiam aut! Quis rerum soluta dolorem iure nihil velit error sequi? Dignissimos accusantium adipisci unde officia? Dolores aut sequi dolorum repellendus quod.&rdquo;</p>
                                         <div className="author">
-                                            <img src="default/img/person_2.jpg" alt="Image placeholder" className="mb-3" />
+                                            <img src="default/img/person_2.jpg" alt="Image placeholder" className="mb-3" style={{ width: 'auto' }} />
                                             <h4>Geert Green</h4>
                                             <p>CEO, Founder</p>
                                         </div>
@@ -35,7 +35,7 @@ class Staff extends Component {
                                     <blockquote className="testimonial">
                                         <p>&ldquo;Ratione alias iure ab facere quia aliquam dolor et voluptates esse nihil corporis distinctio hic ea quo ducimus autem cum amet. Quos accusamus iusto porro nulla temporibus numquam commodi soluta.&rdquo;</p>
                                         <div className="author">
-                                            <img src="default/img/person_3.jpg" alt="Image placeholder" className="mb-3" />
+                                            <img src="default/img/person_3.jpg" alt="Image placeholder" className="mb-3" style={{ width: 'auto' }} />
                                             <h4>Dennis Roman</h4>
                                             <p>CEO, Founder</p>
                                         </div>
@@ -45,7 +45,7 @@ class Staff extends Component {
                                     <blockquote className="testimonial">
                                         <p>&ldquo;Ad quod aspernatur ipsa. Numquam expedita delectus qui ad explicabo voluptas eos vel reiciendis magnam rerum quaerat quisquam accusantium quae saepe ipsam ullam ut ea molestiae porro. Recusandae veniam maxime.&rdquo;</p>
                                         <div className="author">
-                                            <img src="default/img/person_2.jpg" alt="Image placeholder" className="mb-3" />
+                                            <img src="default/img/person_2.jpg" alt="Image placeholder" className="mb-3" style={{ width: 'auto' }} />
                                             <h4>Geert Green</h4>
                                             <p>CEO, Founder</p>
                                         </div>
